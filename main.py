c4f3698cc236adf47729c462f0b20022f38d341c
BrunooBuranello <130617310+BrunooBuranello@users.noreply.github.com>
2026-08-02 18:54:23 -0300

    feat: implement Excel reading and normalization service

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-from pathlib import Path
-
 from services.excel_service import ExcelService
 
+excel = ExcelService()
 
-excel_service = ExcelService()
+try:
+    file_path = excel.read()
 
-file_path = Path("input") / "Carta Laudo_2026-07-31_17-30-13.xlsx"
 
-data = excel_service.read(file_path)
-
-print(data.head(10))
+except Exception as error:
+    print(f"Aviso: \n{error}")
--- services/excel_service.py
+++ services/excel_service.py
@@ -3,12 +3,40 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 INPUT_DIR = BASE_DIR / 'input'
+DEFAULT_FILE_NAME = "formulario_carta_laudo.xlsx"
+DEFAULT_SHEET_NAME = "Formulario_Carta_Laudo"
 
 
-
+# Class para localização e leitura do excel para gerar o formulario.
 class ExcelService:
 
-    def read(self, file_name: str):
-        file_path = INPUT_DIR / file_name
-        return pd.read_excel(file_path)
 
+    # Valida se o diretorio existe, se o arquivo existe e se o formato esperado esta correto.
+    def file_exists(self, file_name: str = DEFAULT_FILE_NAME) -> Path:
+        file_path = INPUT_DIR / file_name
+        if not INPUT_DIR.is_dir():
+            raise FileNotFoundError(f"Diretorio não encontrado:\n{INPUT_DIR}")
+        if not file_path.is_file():
+            raise FileNotFoundError(f"Arquivo não encontrado:\n{file_path.name}\n"
+                                    f"Caminho procurado:\n{file_path.parent}")
+        if file_path.suffix.lower() != ".xlsx":
+            raise ValueError(f"Arquivo econtrado nao tem a fortamação '.xlsx':\n'{file_path.name}'")
+        return file_path
+
+
+    # Normalizar colunas do excel.
+    def normalize_columns(self, df: pd.DataFrame) -> pd.DataFrame:
+        df.columns = (
+            df.columns
+            .str.strip()
+            .str.lower()
+            .str.replace(' ', '_'))
+        return df
+
+
+    # Leitura do DF, chama o localizador, leitura do excel, e tratamento das colunas.
+    def read(self) -> pd.DataFrame:
+        file_path = self.file_exists()
+        df = pd.read_excel(file_path,sheet_name=DEFAULT_SHEET_NAME)
+        df = self.normalize_columns(df)
+        return df
